3f59593cf2a315a865b8e187243855d150630d7d
DKim <kimgetsu@gmail.com>
2026-05-20 15:13:04 +0300

    Пайплайн2

--- vitest.config.js
+++ vitest.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
@@ -7,5 +8,10 @@ export default defineConfig({
     environment: 'happy-dom',
     globals: true,
     setupFiles: ['./tests/config/setup.ts'],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
